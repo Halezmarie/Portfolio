efa4fe6e8c4ae1560d614f8044243cd4e7418305
halezmarie <teacherhaleymarie@gmail.com>
2022-01-25 20:26:05 -0600

    Updates

---
+++ components/About/About.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    'I am a Full Stack Software Engineer experienced in Ruby, JavaScript, React and a background in teaching. A love of problem-solving brought me to web development. My organizational and project-planning skills, coupled with an attention to detail and the ability to communicate technical concepts to non-technical people, will help with both client interaction and driving project completion.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    'My tech skills: Ruby, Rails, SQL, JavaScript, React, JSON, Sinatra, Bootstrap, Redux Thunk, HTML, Git, CSS, ActiveRecord, Object-Oriented Programming, and Microsoft Visual Studio Code'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'Outside of coding, I enjoy gaming, reading, and playing with my puppy, Biscotto. '}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ mock/data.js
@@ -0,0 +1,94 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: '',
+  subtitle: '',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://docs.google.com/document/d/e/2PACX-1vT_BFLne4muZN_KPKXdN5w5mKnptVIfFUgQwueTSXuohR8zlvFgnKege-JaUQ9TSqayn-5KZkKDjuup/pub', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Pups R Us 🐕',
+    info: 'Pups R Us is an app for reviewing toys for your favorite pups! Users can submit reviews for existing toys, or add new toys to the website.',
+    info2: '',
+    url: 'https://www.youtube.com/watch?v=kGQkVHi1O_U',
+    repo: 'https://github.com/Halezmarie/pupsrus-frontend', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project2.png',
+    title: 'Boogle👻',
+    info: '',
+    info2: '',
+    url: 'https://www.youtube.com/watch?v=enG8KTtPc50',
+    repo: 'https://github.com/Halezmarie/Boogle', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project3.png',
+    title: 'TeaScape 🍵',
+    info: 'TeaScape is for anyone who loves tea. Users can create, delete, edit, and review tea. Users can also see the reviews of other users.',
+    info2: '',
+    url: 'https://www.youtube.com/watch?v=ygHHl_Nc8ks',
+    repo: 'https://github.com/Halezmarie/TeaScape-Project', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'haleymgrassi@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'youtube',
+      url: 'https://www.youtube.com/channel/UC9jObIjQp5WJw_cJLYFWQzw',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/haley-grassi0716/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/Halezmarie',
+    },
+    {
+      id: nanoid(),
+      name: 'medium',
+      url: 'https://medium.com/@haleymgrassi',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
